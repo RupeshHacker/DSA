1aaf423101a29301228cac4c1082c756cd637d18
RupeshHacker <rupeshpowar1234@gmail.com>
2024-03-22 22:28:24 +0530

    implementing simple stack

---
+++ Simple-Stack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+void push(char[], char);
+char pop(char[]);
+int top =-1;
+
+main(){
+    char expr[50],ele,st[50];
+    int i = 0,c = 0;
+    
+    std::cout<<"Enter the expression: ";
+    while((expr[i++] = getchar()) != '\n');
+    expr[--i] = '\0';
+
+    for(i = 0; expr[i] != '\0'; i++){
+        if(expr[i] == '(' || expr[i] == '{' || expr[i] == '['){
+            push(st,expr[i]);
+            c++;
+        }
+        else if(expr[i] == ')' || expr[i] == '}' || expr[i] == ']'){
+            ele = pop(st);
+            if((expr[i] == ')' && ele == '(') || (expr[i] == '}' && ele == '{') || (expr[i] == ']' && ele == '[')){
+                c--;
+            }
+        }
+    }
+}
+
+void push(char st[], char ele){
+    st[++top] = ele;
+}
+
+char pop(char st[]){
+    return st[top--];
+}
